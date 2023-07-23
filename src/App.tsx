21ae941cd1ec162ed2c791739046f0c952d6c76a
Jared Franzone <jffranzo@amazon.com>
2023-07-23 13:45:40 -0700

    feat: add authenticated backend call

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { API } from 'aws-amplify';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Jared TS: Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    res: 0
+  };
+
+  async componentDidMount() {
+    const res = await API.post('Endpoint', '/router', {
+      body: {
+        key1: '2',
+        key2: '3',
+        // ...
+      },
+    });
+    console.log("API CALL");
+    console.log(JSON.stringify(res));
+    console.log("response.body " + res);
+    this.setState({ res: res });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Jared TS: Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <p>2 + 3 = {this.state.res}</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -5,6 +5,16 @@ const awsconfig = {
       userPoolId: 'us-east-1_AA5yTAvmw',
       userPoolWebClientId: '3pipop8gkjj6vcrud7k8nj66fq',
     },
+    API: {
+        endpoints: [
+            {
+                name: "Endpoint", // name of the API in API Gateway console
+                endpoint: "https://8351pwpo0k.execute-api.us-east-1.amazonaws.com/prod",
+                region: "us-east-1",
+                paths: ['/']
+            }
+        ]
+    }
   };
   
   export default awsconfig;

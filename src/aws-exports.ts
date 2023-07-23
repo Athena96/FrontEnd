eb8c228628bdd6c853a3d643e02fef384f351b0e
Jared Franzone <jffranzo@amazon.com>
2023-07-23 12:17:33 -0700

    feat: add user accounts

---
+++ src/aws-exports.ts
@@ -0,0 +1,10 @@
+// src/aws-exports.js
+const awsconfig = {
+    Auth: {
+      region: 'us-east-1',
+      userPoolId: 'us-east-1_AA5yTAvmw',
+      userPoolWebClientId: '3pipop8gkjj6vcrud7k8nj66fq',
+    },
+  };
+  
+  export default awsconfig;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Amplify from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.Amplify.configure(awsExports);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+  <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+         {user &&  <h1>Hello {user.username}</h1> }
+          <App/>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   </React.StrictMode>
 );
 
